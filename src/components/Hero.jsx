15cb7ff0abc1c6cb18404be67001ff844928a469
JaccobStanton <jstanton23@my.stlcc.edu>
2025-01-22 11:32:48 -0600

    Updated syntax of the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ export function Hero() {
             />
             <div className="relative">
               <p className="inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
-                Planting Precision with Sorghum Science.
+                Predicting Sorghum Success.
               </p>
               <p className="mt-3 text-2xl tracking-tight text-slate-400">
                 Dive into a hands-on research experience to uncover the optimal
