f6810e63a238627c23988fbc47a8b17f4af852ea
JaccobStanton <jstanton23@my.stlcc.edu>
2025-07-15 10:34:10 -0500

    Updated config file missing needed import statement

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
+// next.config.mjs
+import path from 'path'
 import withMarkdoc from '@markdoc/next.js'
-
 import withSearch from './src/markdoc/search.mjs'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src')
+    return config
+  },
 }
 
 export default withSearch(
